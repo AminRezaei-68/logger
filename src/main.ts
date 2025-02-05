023ce896460171de8bb2242dc7e9ab85b675559a
AminRezaei-68 <rezaei.amin@gmail.com>
2025-02-05 10:40:57 +0330

    upgrade app to micro

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,11 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: { host: 'localhost', port: 3001 },
-  });
-  await app.startAllMicroservices();
-  const port = process.env.PORT || 3002;
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}/api`);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    { transport: Transport.TCP, options: { host: 'localhost', port: 5550 } },
+  );
+
+  await app.listen();
 }
 bootstrap();
